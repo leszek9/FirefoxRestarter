1e5ec17dfd3b1b4dc0ddc68dce8919e21bf20241
ponuryrolnik <lecho9@gmail.com>
2020-06-12 17:05:26 +0200

    Add files via upload

--- ffrestarter.py
+++ ffrestarter.py
@@ -10,13 +10,13 @@ browserExe = "firefox.exe"
 
 def ffrestart():
     while True:
-        print("\nClosing Firefox\n")
+        print("\nClosing Firefox...\n")
         os.system("taskkill /f /im "+browserExe)
         print("\nWaiting 5 seconds...\n")
         time.sleep(5)
         p = subprocess.Popen(['C:\Program Files\Mozilla Firefox\\firefox.exe'], shell=True, stdout=subprocess.PIPE,stderr=subprocess.STDOUT, bufsize=0)
         print(p)
-        print("\nWait 1 hour for next Restart\n")
+        print("\nWait 1 hour for next Restart...\n")
         time.sleep(3600)
 
 
